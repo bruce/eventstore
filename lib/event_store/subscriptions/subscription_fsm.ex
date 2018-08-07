65e42f000ed3c1e0a90e4efa2ff30154321158ce
Ben Smith <ben@10consulting.com>
2018-08-07 23:51:25 +0100

    Subscription should notifying pending events after all filtered
    Fix bug when all events in a batch are filtered by the subscriptions `selector/1` function the event are acknowledged but subsequent pending events are not sent to the subscriber.
    
    Fixes #128

--- lib/event_store/subscriptions/subscription_fsm.ex
+++ lib/event_store/subscriptions/subscription_fsm.ex
@@ -394,9 +394,9 @@ defmodule EventStore.Subscriptions.SubscriptionFsm do
         # Subscriber has ack'd last received event, so send pending.
         [initial | pending] = pending_events
 
-        data = notify_subscriber(initial, data)
+        data = %SubscriptionState{data | pending_events: pending}
 
-        %SubscriptionState{data | pending_events: pending}
+        notify_subscriber(initial, data)
 
       _ ->
         # Subscriber has not yet ack'd last received event, don't send any more.
@@ -415,8 +415,10 @@ defmodule EventStore.Subscriptions.SubscriptionFsm do
 
     case filter(events, data) do
       [] ->
-        # All events filtered, so just ack last event.
-        ack_events(data, last_event_number(events))
+        # All events filtered, so just ack last event and continue notifying pending events.
+        data
+        |> ack_events(last_event_number(events))
+        |> notify_pending_events()
 
       events_to_send ->
         # Send filtered & mapped events to subscriber
@@ -455,7 +457,7 @@ defmodule EventStore.Subscriptions.SubscriptionFsm do
 
     case Enum.member?(filtered_event_numbers, ack + 1) do
       true ->
-        # Next event was filtered, attempt
+        # Next event was filtered, attempt to acknowledge the one after next
         ack_events(data, ack + 1)
 
       false ->
